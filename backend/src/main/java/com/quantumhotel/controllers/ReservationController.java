837c459c362bf1149e28b0dc1155aa4e462928b5
matija-tusek <matija.tusek@fer.hr>
2026-01-10 19:09:26 +0100

    Fixed user principal equals null for ReservationController, fixed ArticleRequest DTO
    Added distinction between OAuth2 and user login for ReservationController.

--- backend/src/main/java/com/quantumhotel/controllers/ReservationController.java
+++ backend/src/main/java/com/quantumhotel/controllers/ReservationController.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.Authentication;
 import com.quantumhotel.services.ReservationService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipal;
@@ -29,28 +31,42 @@ public class ReservationController {
 
     @GetMapping("/me")
     public List<ReservationListDTO> myReservations(
-            @AuthenticationPrincipal UserDetails principal
-    ) {
-        return reservationService.findMine(principal.getUsername());
+            Authentication authentication)
+    {
+        return reservationService.findMine(extractUsername(authentication));
     }
 
     @PostMapping
     public ReservationListDTO create(
             @RequestBody ReservationCreateDTO dto,
-            @AuthenticationPrincipal UserDetails principal
+            Authentication authentication
     ) {
         return ReservationListDTO.from(
-                reservationService.create(dto, principal.getUsername())
+                reservationService.create(dto, extractUsername(authentication))
         );
     }
     @PatchMapping("/{id}")
     public ReservationListDTO patch(
             @PathVariable Long id,
             @RequestBody ReservationPatchDto dto,
-            @AuthenticationPrincipal UserDetails principal
+            Authentication authentication
     ) {
         return ReservationListDTO.from(
-                reservationService.patch(id, dto, principal.getUsername())
+                reservationService.patch(id, dto, extractUsername(authentication))
         );
+    }
+
+    private String extractUsername(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof org.springframework.security.core.userdetails.UserDetails userDetails) {
+            return userDetails.getUsername(); // normal login
+        }
+
+        if (principal instanceof OAuth2User oauth2User) {
+            return oauth2User.getAttribute("email"); // Google OAuth2
+        }
+
+        throw new IllegalStateException("Unsupported principal type: " + principal.getClass());
     }
 }
